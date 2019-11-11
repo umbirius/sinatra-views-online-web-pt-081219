21791fd3f49c97bd979a5f793b93b0688ef06d7e
Umberto Palazzo <upalazzo93@gmail.com>
2019-11-11 15:39:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
+<<<<<<< HEAD
 		erb :index
+=======
+		"<h1>Hello World</h1>"
+>>>>>>> a66d3174b1aac0b09fbd0e1d90dad6f7ec3a3aed
 	end
 	
   get "/info" do
